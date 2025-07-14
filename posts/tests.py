a6631746ffa9d9587445aa4cd65d69fb1fa4d5a7
Asadbek Shahobiddinov <fazogirdevs@gmail.com>
2025-07-14 07:30:39 +0500

    update

--- posts/tests.py
+++ posts/tests.py
@@ -3,13 +3,6 @@ from .models import Post
 
 # Create your tests here.
 
-class PostModelTest(TestCase):
-    def setUp(self):
-        Post.objects.create('Just a test')
-
-    def test_text_content(self):
-        post=Post.objects.get(id=1)
-        expected_object_name
 
 
 
